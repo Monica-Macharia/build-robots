e34159993c94a6f40d6a72c094ce95bcc411f04a
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-05-23 15:19:22 +0300

    robot structure

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import HomePage from './components/HomePage.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+
+  <header>
+    <div class="wrapper">
+      <HomePage />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="home">
+  <div>
+    <img class="robot" src="../assets/robot-home.png" aria-hidden="true" />
+  </div>
+  <div class="get-started">
+    <a href="">Start </a> a robot!
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
